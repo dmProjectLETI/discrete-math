d2e0fd1bf4409d0cdcb38db42e23d54709c6d555
dmProjectLETI <timkuznor@yandex.ru>
2016-04-07 18:21:54 +0300

    Update test_modules.cpp

--- VS-Version/Modules/test_modules.cpp
+++ VS-Version/Modules/test_modules.cpp
@@ -247,7 +247,85 @@ Natural LCM_NN_N(Natural chislo1, Natural chislo2)//Íàõîäèò ÍÎÊ äâó
 // Êîíåö ìîäóëÿ äëÿ íàòóðàëüíûõ
 
 // Ìîäóëü äëÿ öåëûõ
+struct Integer 
+{
+	bool pos = true;
+	Natural A;
+};
 
+int POZ_Z_D(Integer num) //Ïîêàçàòåëü çíàêà, ñèãíàòóðà.
+{
+	if (!num.pos)
+		return -1;
+	else if (NZER_N_B(num.A))
+		return 1;
+	return 0;
+}
+
+Natural ABS_Z_N(Integer num) //Ìîäóëü.
+{
+	num.pos = true;
+	return num;
+}
+
+Integer MUL_Z_NEGZ(Integer num) //Óìíîæåíèå íà -1.
+{
+	num.pos = !num.pos;
+	return num;
+}
+
+Integer ADD_ZZ_Z(Integer first, Integer second) //Ñëîæåíèå äâóõ öåëûõ.
+{
+	Integer sum;
+	if (POZ_Z_D(first) <= 0)
+		if (POZ_Z_D(second) <= 0) {
+			sum = ADD_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+			sum = MUL_Z_NEGZ(sum);
+		}
+		else {
+			if (ABS_Z_N(first) > ABS_Z_N(second)) {
+				sum = MUL_Z_NEGZ(sum);
+			} 
+			sum = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
+		}
+	else {
+		if (POZ_Z_D(second) >= 0)
+			sum = ADD_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		else {
+			if (ABS_Z_N(second) > ABS_Z_N(first)) {
+				sum = MUL_Z_NEGZ(sum);
+			}
+			sum = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		}
+	}
+	return sum;
+}
+
+Integer SUB_ZZ_Z(Integer first, Integer second) //Âû÷èòàíèå äâóõ öåëûõ.
+{
+	Integer sub;
+	sub.pos = 1;
+	if (POZ_Z_D(first) <= 0) 
+		if (POZ_Z_D(second) <= 0) {
+			if (ABS_Z_N(first) > ABS_Z_N(second)) {
+				sum = MUL_Z_NEGZ(sum);
+			}
+			sub = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
+		} else {
+			sub = ADD_ZZ_Z(first, second);
+			sum = MUL_Z_NEGZ(sum);
+		}
+	else {
+		if (POZ_Z_D(second) >= 0) {
+			if (ABS_Z_N(second) > ABS_Z_N(first)) {
+				sum = MUL_Z_NEGZ(sum);
+			}
+			sub = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		}
+		else 
+			sub = ADD_ZZ_Z(first, second);
+	}
+}
 // Êîíåö ìîäóëÿ äëÿ öåëûõ
 
 // Ìîäóëü äëÿ äðîáåé
