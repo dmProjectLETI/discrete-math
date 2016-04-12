4374f80d3b12718cda0570129b800685b48aec88
dmProjectLETI <timkuznor@yandex.ru>
2016-04-13 01:47:42 +0300

    Update Modul_Ratio.cpp

--- VS-Version/Modules/Q/Modul_Ratio.cpp
+++ VS-Version/Modules/Q/Modul_Ratio.cpp
@@ -17,42 +17,27 @@ Integer TRAN_Q_Z(Ratio b) // Преобразование дробного в ц
 
 Ratio TRAN_Z_Q(Integer A) // Преобразование целого в дробное
 {
-	Ratio B; //дробь, которую возвращает функция
+	Ratio B;//дробь, которую возвращает функция
 	B.num = A; //число А в числитель
 	B.denum.a.push_back(1); //в знаменатель ставим 1
 	return B;
 }
 
-Ratio RED_Q_Q(Ratio A) // Функция сокращения дроби
-{
-	Natural B = GVF_NN_N(A.num.A, A.denum); // Находим НОД от |числителя| и знаменателя
-	A.num.A = DIV_ZZ_Z(A.num.A, B); // Делим числитель на НОД
-	A.denum = ABS_Z_N(DIV_ZZ_Z(A.denum, B)); // Делим знаменатель на НОД
-	return A;
-}
 
-bool INT_Q_Z(Ratio R) //Проверка на целое, если рациональное число является целым, то «да», иначе «нет»
+
+bool INT_Q_Z(Ratio R)//Проверка на целое, если рациональное число является целым, то «да», иначе «нет»
 {
 	return  (R.denum.A.size() == 1 && R.denum.A[0] == 1) ? 1 : 0; //Если размер знаменателя и первый его элемент равны 1 - функция возвращает 1, иначе - 0
 }
 Ratio MUL_QQ_Q (Ratio A, Ratio B) //умножение дробных чисел
 {
- A.num = MUL_ZZ_Z(A.num.A, B.num.A);    // перемножение числителей
- A.denum = MUL_ZZ_Z(A.denum, B.denum); // перемножение знаменателей 
+ A.num = MUL_ZZ_Z(A.num, B.num);    // перемножение числителей
+ A.denum = MUL_NN_N(A.denum, B.denum); // перемножение знаменателей 
  return RED_Q_Q(A);                   // возвращается сокращенная дробь
 }
 
-
-Ratio DIV_QQ_Q (Ratio A, Ratio B) //деление дробных чисел
-{  
- Ratio C;
- C.num = MUL_ZZ_Z(A.num.A, B.denum);
- C.denum = MUL_ZZ_Z(A.denum, B.num.A);
- return RED_Q_Q(C);
-}
-
 Ratio SUB_QQ_Q(Ratio A, Ratio B) //Вычитание дробей
-	{
+{
 	Ratio C;
 	Natural temp = LCM_NN_N(A.denum, B.denum); // Поиск НОК
 	A.num.A = MUL_NN_N(A.num.A, DIV_NN_N(temp, A.denum));
@@ -60,4 +45,27 @@ Ratio SUB_QQ_Q(Ratio A, Ratio B) //Вычитание дробей
 	C.num = SUB_ZZ_Zs(A.num, B.num);
 	C.denum = temp;
 	return C;
+}
+
+Ratio RED_Q_Q(Ratio A) // Функция сокращения дроби
+{
+	Natural B = GCF_NN_N(A.num.A, A.denum); // Находим НОД от |числителя| и знаменателя
+	A.num.A = DIV_NN_N(A.num.A, B); // Делим числитель на НОД
+	A.denum = DIV_NN_N(A.denum, B)); // Делим знаменатель на НОД
+	return A;
+}
+Ratio DIV_QQ_Q (Ratio A, Ratio B) //деление дробных чисел
+{  
+ Ratio C;
+ Integer G ;
+ G.A = B.denum;
+ G.b = 0;
+ C.num = MUL_ZZ_Z(A.num, G);
+ C.denum = MUL_NN_N(A.denum,B.num.A)
+if (B.num.b == 1)
+    if(C.num.b == 1)
+        C.num.b = 0;
+    else
+        C.num.b = 1;
+ return RED_Q_Q(C);
 }
