3deb3c2756a7dc51daf27cfb1f15988b16de5df5
dmProjectLETI <timkuznor@yandex.ru>
2016-04-12 21:47:53 +0300

    Update integer.cpp

--- VS-Version/TestUnit/integer.cpp
+++ VS-Version/TestUnit/integer.cpp
@@ -1,93 +1,130 @@
 #include "integer.h"
 #include "natural.h"
+#include <iostream>
 
-int POZ_Z_D(Integer num) //Определение знака целого числа.
-{
-    if (!num.b)
-        return -1;
-    else if (NZER_N_B(num.A))
-        return 1;
-    return 0;
-}
+using namespace std;
 
-Natural ABS_Z_N(Integer num) //Модуль.
+struct Integer
 {
-    return num.A;
-}
-
-Integer MUL_Z_NEGZ(Integer num) //Умножение целого на -1.
-{
-    num.b = !num.b;
-    return num;
-}
-
-//DO NOT WORK
-Integer ADD_ZZ_Z(Integer first, Integer second) //Сложение двух целых.
-{
-    Integer sum;
-    if (POZ_Z_D(first) <= 0)
-        if (POZ_Z_D(second) <= 0) {
-            sum.A = ADD_NN_N(ABS_Z_N(first), ABS_Z_N(second));
-            sum = MUL_Z_NEGZ(sum);
-        }
-        else {
-            if (first.A > second.A) {
-                sum = MUL_Z_NEGZ(sum);
-            }
-            sum.A = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
-        }
-    else
-        if (POZ_Z_D(second) >= 0)
-            sum.A = ADD_NN_N(ABS_Z_N(first), ABS_Z_N(second));
-        else {
-            if (second.A > first.A) {
-                sum = MUL_Z_NEGZ(sum);
-            }
-            sum.A = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
-        }
-    return sum;
-}
-
-//DO NOT WORK
-Integer SUB_ZZ_Z(Integer first, Integer second) //Вычитание двух целых.
-{
-    Integer sub;
-    sub.b = 1;
-    if (POZ_Z_D(first) <= 0)
-        if (POZ_Z_D(second) <= 0) {
-            if (first.A > second.A) {
-                sum = MUL_Z_NEGZ(sum);
-            }
-            sub.A = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
-        } else {
-            sub.A = ADD_ZZ_Z(first, second);
-            sum = MUL_Z_NEGZ(sum);
-        }
-    else {
-        if (POZ_Z_D(second) >= 0) {
-            if (second.A > first.A) {
-                sum = MUL_Z_NEGZ(sum);
-            }
-            sub.A = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
-        }
-        else
-            sub.A = ADD_ZZ_Z(first, second);
-    }
-    return sub;
-}
-
-//DO NOT WORK
-Integer MOD_ZZ_Z(Integer first, Integer second) //Остаток от деления двух целых
-{
-    Integer result;
-    result.b = 1;
-    if (NZER_N_B(second.A)) //If divisor is not equal to 0
-    {
-        result.A = MOD_NN_N(first.A, second.A);
-        if ((POZ_Z_D(first) < 0)) //If dividend is negative
-            result.A = SUB_ZZ_Z(second.A, result.A); // If a < 0 then a mod b = |a|- (|a| mod |b|)
-        return result;
-        }
-    else //If divisor is equal to 0
-        throw invalid_argument("Divided by 0");
+	bool pos = true;
+	Natural A;
 };
+
+int POZ_Z_D(Integer num) //Ïîêàçàòåëü çíàêà, ñèãíàòóðà.
+{
+	if (!num.pos)
+		return -1;
+	else if (NZER_N_B(num.A))
+		return 1;
+	return 0;
+}
+
+Integer TRANS_N_Z(Natural num) 
+{
+	Integer newNum;
+	newNum.A = num;
+	return newNum;
+}
+
+Natural ABS_Z_N(Integer num) //Ìîäóëü.
+{
+	return num.A;
+}
+
+Integer MUL_Z_NEGZ(Integer num) //Óìíîæåíèå íà -1.
+{
+	num.pos = !num.pos;
+	return num;
+}
+
+Integer ADD_ZZ_Z(Integer first, Integer second) //Ñëîæåíèå äâóõ öåëûõ.
+{
+	Integer sum;
+	if (POZ_Z_D(first) <= 0)
+		if (POZ_Z_D(second) <= 0) {
+			sum.A = ADD_NN_N(first.A, second.A);
+			sum = MUL_Z_NEGZ(sum);
+		}
+		else {
+			if (!COM_NN_D(first.A, second.A)) {
+				sum = MUL_Z_NEGZ(sum);
+			}
+			sum.A = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
+		}
+	else
+		if (POZ_Z_D(second) >= 0)
+			sum.A = ADD_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		else {
+			if (COM_NN_D(first.A, second.A)) {
+				sum = MUL_Z_NEGZ(sum);
+			}
+			sum.A = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		}
+		return sum;
+}
+
+Integer SUB_ZZ_Z(Integer first, Integer second) //Âû÷èòàíèå äâóõ öåëûõ.
+{
+	Integer sub;
+	sub.pos = 1;
+	if (POZ_Z_D(first) <= 0)
+		if (POZ_Z_D(second) <= 0) {
+			if (COM_NN_D(first.A, second.A)) {
+				sub = MUL_Z_NEGZ(sub);
+			}
+			sub.A = SUB_NN_N(ABS_Z_N(second), ABS_Z_N(first));
+		}
+		else {
+			sub.A = ADD_ZZ_Z(first, second).A;
+			sub.pos = 0;
+		}
+	else {
+		if (POZ_Z_D(second) >= 0) {
+			if (!COM_NN_D(first.A, second.A)) {
+				sub = MUL_Z_NEGZ(sub);
+			}
+			sub.A = SUB_NN_N(ABS_Z_N(first), ABS_Z_N(second));
+		}
+		else
+			sub.A = ADD_ZZ_Z(first, second).A;
+	}
+	return sub;
+}
+
+Integer MUL_ZZ_Z(Integer first, Integer second) {
+	Integer mul;
+	mul.A = MUL_NN_N(first.A, second.A);
+
+	if (POZ_Z_D(first) == -1)
+		MUL_Z_NEGZ(mul);
+	if (POZ_Z_D(second) == -1)
+		MUL_Z_NEGZ(mul);
+
+	return mul;
+}
+
+Integer MOD_ZZ_Z(Integer first, Natural second)
+{
+	Integer result;
+
+		result.A = MOD_NN_N(first.A, second);
+		if ((POZ_Z_D(first) < 0)) //If dividend is negative
+			result = SUB_ZZ_Z(TRANS_N_Z(second), TRANS_N_Z(result.A)); // If a < 0 then a mod b = |a|- (|a| mod |b|)
+		return result;
+};
+
+Integer DIV_ZZ_Z(Integer first, Integer second) //Частное от деления большего целого числа на меньшее 
+											 //или равное натуральное с остатком (делитель отличен от нуля)
+{
+	Integer div;
+
+	div.A=DIV_NN_N(first.A, second.A);
+
+	if (POZ_Z_D(first) == -1)
+		MUL_Z_NEGZ(div);
+	if (POZ_Z_D(second) == -1)
+		MUL_Z_NEGZ(div);
+
+	return div;
+}
+
