40149f675bbd747390b7eca0742aaec02ffba0a8
dmProjectLETI <timkuznor@yandex.ru>
2016-04-13 01:46:33 +0300

    Update test_modules.cpp

--- VS-Version/Modules/test_modules.cpp
+++ VS-Version/Modules/test_modules.cpp
@@ -406,13 +406,7 @@ Ratio TRAN_Z_Q(Integer A) // Преобразование целого в дро
 	return B;
 }
 
-Ratio RED_Q_Q(Ratio A) // Функция сокращения дроби
-{
-	Natural B = GVF_NN_N(A.num.A, A.denum); // Находим НОД от |числителя| и знаменателя
-	A.num.A = DIV_ZZ_Z(A.num.A, B); // Делим числитель на НОД
-	A.denum = ABS_Z_N(DIV_ZZ_Z(A.denum, B)); // Делим знаменатель на НОД
-	return A;
-}
+
 
 bool INT_Q_Z(Ratio R)//Проверка на целое, если рациональное число является целым, то «да», иначе «нет»
 {
@@ -420,13 +414,13 @@ bool INT_Q_Z(Ratio R)//Проверка на целое, если рациона
 }
 Ratio MUL_QQ_Q (Ratio A, Ratio B) //умножение дробных чисел
 {
- A.num = MUL_ZZ_Z(A.num.A, B.num.A);    // перемножение числителей
- A.denum = MUL_ZZ_Z(A.denum, B.denum); // перемножение знаменателей 
+ A.num = MUL_ZZ_Z(A.num, B.num);    // перемножение числителей
+ A.denum = MUL_NN_N(A.denum, B.denum); // перемножение знаменателей 
  return RED_Q_Q(A);                   // возвращается сокращенная дробь
 }
 
 Ratio SUB_QQ_Q(Ratio A, Ratio B) //Вычитание дробей
-	{
+{
 	Ratio C;
 	Natural temp = LCM_NN_N(A.denum, B.denum); // Поиск НОК
 	A.num.A = MUL_NN_N(A.num.A, DIV_NN_N(temp, A.denum));
@@ -435,11 +429,27 @@ Ratio SUB_QQ_Q(Ratio A, Ratio B) //Вычитание дробей
 	C.denum = temp;
 	return C;
 }
+
+Ratio RED_Q_Q(Ratio A) // Функция сокращения дроби
+{
+	Natural B = GCF_NN_N(A.num.A, A.denum); // Находим НОД от |числителя| и знаменателя
+	A.num.A = DIV_NN_N(A.num.A, B); // Делим числитель на НОД
+	A.denum = DIV_NN_N(A.denum, B)); // Делим знаменатель на НОД
+	return A;
+}
 Ratio DIV_QQ_Q (Ratio A, Ratio B) //деление дробных чисел
 {  
  Ratio C;
- C.num = MUL_ZZ_Z(A.num.A, B.denum);
- C.denum = MUL_ZZ_Z(A.denum, B.num.A);
+ Integer G ;
+ G.A = B.denum;
+ G.b = 0;
+ C.num = MUL_ZZ_Z(A.num, G);
+ C.denum = MUL_NN_N(A.denum,B.num.A)
+if (B.num.b == 1)
+    if(C.num.b == 1)
+        C.num.b = 0;
+    else
+        C.num.b = 1;
  return RED_Q_Q(C);
 }
 
