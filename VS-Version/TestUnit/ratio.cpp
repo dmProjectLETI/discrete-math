67fd92e3a83f041cc39c2ad398694c9620b943d5
dmProjectLETI <timkuznor@yandex.ru>
2016-04-13 01:42:55 +0300

    Update ratio.cpp

--- VS-Version/TestUnit/ratio.cpp
+++ VS-Version/TestUnit/ratio.cpp
@@ -1,43 +1,73 @@
 #include "ratio.h"
 
-// Функция сложения двух рациональных чисел
-Ratio ADD_QQ_Q(Ratio A, Ratio B)
+Ratio ADD_QQ_Q(Ratio A, Ratio B)// Сложение дробей
 {
-    Ratio C;
-    Natural temp = LCM_NN_N(A.denum, B.denum); // Ïîèñê ÍÎÊ
-    A.num.A = MUL_NN_N(A.num.A, DIV_NN_N(temp, A.denum));
-    B.num.A = MUL_NN_N(B.num.A, DIV_NN_N(temp, B.denum));
-    C.num = ADD_ZZ_Z(A.num, B.num);
-    C.denum = temp;
-    return C;
+	Ratio C;
+	Natural temp = LCM_NN_N(A.denum, B.denum); // Поиск НОК
+	A.num.A = MUL_NN_N(A.num.A, DIV_NN_N(temp, A.denum));
+	B.num.A = MUL_NN_N(B.num.A, DIV_NN_N(temp, B.denum));
+	C.num = ADD_ZZ_Z(A.num, B.num);
+	C.denum = temp;
+	return C;
 }
 
-// Функция, которая преобразует целое в дробное(со знаменателем = 1)
-Integer TRAN_Q_Z(Ratio b)
+Integer TRAN_Q_Z(Ratio b) // Преобразование дробного в целое (Если знаменатель равен 1)
+						  // Перед вызовом следует убедиться, что знаменатель равен единице
 {
-    return(b.num); // Ãîñïîæè, êàê æå ýòî ñëîæíî áûëî
+	return(b.num); 
 }
 
-// Функция сокращения дроби
-Ratio TRAN_Z_Q(Integer A)
+Ratio TRAN_Z_Q(Integer A) // Преобразование целого в дробное
 {
-    Ratio B; //äðîáü, êîòîðóþ âîçâðàùàåò ôóíêöèÿ
-    B.num = A; //÷èñëî À â ÷èñëèòåëü
-    B.denum.a.push_back(1); //â çíàìåíàòåëü ñòàâèì 1
-    return B;
+	Ratio B;//дробь, которую возвращает функция
+	B.num = A; //число А в числитель
+	B.denum.a.push_back(1); //в знаменатель ставим 1
+	return B;
 }
 
-// Преобразование дробного в целое
-Ratio RED_Q_Q(Ratio A)
+
+
+bool INT_Q_Z(Ratio R)//Проверка на целое, если рациональное число является целым, то «да», иначе «нет»
 {
-    Natural B = GVF_NN_N(A.num.A, A.denum); // Íàõîäèì ÍÎÄ îò |÷èñëèòåëÿ| è çíàìåíàòåëÿ
-    A.num.A = DIV_ZZ_Z(A.num.A, B); // Äåëèì ÷èñëèòåëü íà ÍÎÄ
-    A.denum = DIV_ZZ_Z(A.denum, B); // Äåëèì çíàìåíàòåëü íà ÍÎÄ
-    return A;
+	return  (R.denum.A.size() == 1 && R.denum.A[0] == 1) ? 1 : 0; //Если размер знаменателя и первый его элемент равны 1 - функция возвращает 1, иначе - 0
+}
+Ratio MUL_QQ_Q (Ratio A, Ratio B) //умножение дробных чисел
+{
+ A.num = MUL_ZZ_Z(A.num, B.num);    // перемножение числителей
+ A.denum = MUL_NN_N(A.denum, B.denum); // перемножение знаменателей 
+ return RED_Q_Q(A);                   // возвращается сокращенная дробь
 }
 
-// Если рациональное число - целое, то 1, иначе 0
-bool INT_Q_Z(Ratio R)
+Ratio SUB_QQ_Q(Ratio A, Ratio B) //Вычитание дробей
 {
-    return  (R.denum.A.size() == 1 && R.denum.A[0] == 1) ? 1 : 0; //Åñëè ðàçìåð çíàìåíàòåëÿ è ïåðâûé åãî ýëåìåíò ðàâíû 1 - ôóíêöèÿ âîçâðàùàåò 1, èíà÷å - 0
+	Ratio C;
+	Natural temp = LCM_NN_N(A.denum, B.denum); // Поиск НОК
+	A.num.A = MUL_NN_N(A.num.A, DIV_NN_N(temp, A.denum));
+	B.num.A = MUL_NN_N(B.num.A, DIV_NN_N(temp, B.denum));
+	C.num = SUB_ZZ_Zs(A.num, B.num);
+	C.denum = temp;
+	return C;
+}
+
+Ratio RED_Q_Q(Ratio A) // Функция сокращения дроби
+{
+	Natural B = GCF_NN_N(A.num.A, A.denum); // Находим НОД от |числителя| и знаменателя
+	A.num.A = DIV_NN_N(A.num.A, B); // Делим числитель на НОД
+	A.denum = DIV_NN_N(A.denum, B)); // Делим знаменатель на НОД
+	return A;
+}
+Ratio DIV_QQ_Q (Ratio A, Ratio B) //деление дробных чисел
+{  
+ Ratio C;
+ Integer G ;
+ G.A = B.denum;
+ G.b = 0;
+ C.num = MUL_ZZ_Z(A.num, G);
+ C.denum = MUL_NN_N(A.denum,B.num.A)
+if (B.num.b == 1)
+    if(C.num.b == 1)
+        C.num.b = 0;
+    else
+        C.num.b = 1;
+ return RED_Q_Q(C);
 }
