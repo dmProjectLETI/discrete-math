936c4ea21bd43363cda6fb0c668483d34ae9bae5
dmProjectLETI <timkuznor@yandex.ru>
2016-04-21 17:48:09 +0300

    Update test_modules.cpp

--- VS-Version/Modules/test_modules.cpp
+++ VS-Version/Modules/test_modules.cpp
@@ -245,7 +245,7 @@ Natural LCM_NN_N(Natural chislo1, Natural chislo2)//Находит НОК дву
  //начало модуля для целых чисел
 
 int POZ_Z_D(Integer num) //Сигнатура, определение знака целого числа.	
-{			 //Выполнил Предтеченский Дмитрий.
+{			 //Выполнил Предтеченский Дмитрий (5396).
 	if (!num.pos)
 		return -1;
 	else if (NZER_N_B(num.A))
@@ -254,30 +254,30 @@ int POZ_Z_D(Integer num) //Сигнатура, определение знака
 }
 
 Integer TRANS_N_Z(Natural num) //Преобразование натурального числа в целое число.
-{			       //Выполнил Предтеченский Дмитрий.	
+{			       //Выполнил Предтеченский Дмитрий (5396).	
 	Integer newNum;
 	newNum.A = num;
 	return newNum;
 }
 
 Natural TRANS_Z_N(Integer num) //Преобразование целого положительного числа в натуральное число.
-{			       //Выполнил Предтеченский Дмитрий.	
+{			       //Выполнил Предтеченский Дмитрий (5396).	
 	return num.A;
 }
 
 Natural ABS_Z_N(Integer num) //Модуль целого числа.
-{			     //Выполнил Предтеченский Дмитрий.
+{			     //Выполнил Предтеченский Дмитрий (5396).
 	return num.A;
 }
 
 Integer MUL_Z_NEGZ(Integer num) //Умножение целого числа на -1
-{				//Выполнил Предтеченский Дмитрий.
+{				//Выполнил Предтеченский Дмитрий (5396).
 	num.pos = !num.pos;
 	return num;
 }
 
 Integer ADD_ZZ_Z(Integer first, Integer second) //Сложение двух целых чисел.
-{						//Выполнил Предтеченский Дмитрий.
+{						//Выполнил Предтеченский Дмитрий (5396).
 	Integer sum;
 
 	if (((first.pos == 1) && (second.pos == 1)) || ((first.pos == 0) && (second.pos == 0)))
@@ -311,7 +311,7 @@ Integer ADD_ZZ_Z(Integer first, Integer second) //Сложение двух це
 }
 
 Integer SUB_ZZ_Z(Integer first, Integer second) //Вычитание двух целых чисел.
-{						//Выполнил Предтеченский Дмитрий.
+{						//Выполнил Предтеченский Дмитрий (5396).
 	Integer sub;
 
 	if (POZ_Z_D(first) <= 0)
@@ -343,7 +343,7 @@ Integer SUB_ZZ_Z(Integer first, Integer second) //Вычитание двух ц
 }
 
 Integer MUL_ZZ_Z(Integer first, Integer second) //Умножение двух целых чисел.
-{						//Выполнил Предтеченский Дмитрий.
+{						//Выполнил Предтеченский Дмитрий (5396).
 	Integer mul;
 	mul.A = MUL_NN_N(first.A, second.A);
 
@@ -356,7 +356,7 @@ Integer MUL_ZZ_Z(Integer first, Integer second) //Умножение двух ц
 }
 
 Integer MOD_ZZ_Z(Integer first, Natural second) //Остаток от деления целого числа на целое число. 
-{						//Выполнила Чаркова Дарья.
+{						//Выполнила Чаркова Дарья (5396).
 	Integer result;
 
 	result.A = MOD_NN_N(first.A, second);
@@ -366,7 +366,7 @@ Integer MOD_ZZ_Z(Integer first, Natural second) //Остаток от делен
 
 Integer DIV_ZZ_Z(Integer first, Natural second) //Частное от деления большего целого числа на меньшее 
 						//или равное натуральное число с остатком (делитель отличен от нуля).
-{						//Выполнил Уразаев Евгений.
+{						//Выполнил Уразаев Евгений (5396).
 	Integer div;
 
 	div.A = DIV_NN_N(first.A, second);
